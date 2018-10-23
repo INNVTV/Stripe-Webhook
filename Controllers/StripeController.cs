dc263000407e06edb79bb654a2e838e6a0d12cf9
Kaz <ka@innvtv.com>
2018-10-23 11:12:21 -0700

    Stubbed out controller

--- Controllers/StripeController.cs
+++ Controllers/StripeController.cs
@@ -14,9 +14,39 @@ namespace StripeWebhook.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post(Stripe.Event stripeEvent)
         {
-        }
+            try{
 
+                // Unpack the stripe event
+                string stripeEventId = stripeEvent.Id;
+
+                // Ensure idempotency (Has this already been logged?)
+
+                switch(stripeEvent.Type)
+                {
+                    case "invoice.payment_succeeded":
+                        break;
+                    case "invoice...d":
+                        break;
+                    default:
+                        break;
+                }
+
+                // Log into message queue:
+
+                // Return status code 200
+                return Ok();
+
+            }
+            catch(Exception e)
+            {
+                // Log the exception:
+                var exceptionMessage=e.Message;
+
+                // Return status code 400:
+                return BadRequest();
+            }     
+        }
     }
 }
