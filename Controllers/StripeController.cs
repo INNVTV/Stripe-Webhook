cb46465056daca6cb82f16ada29526eb18d3e10c
Kaz <ka@innvtv.com>
2018-10-23 10:49:48 -0700

    Added Docker files

---
+++ Controllers/StripeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Stripe;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StripeWebhook.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class StripeController : ControllerBase
+    {
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
